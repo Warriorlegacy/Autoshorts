59b8c4319d1f0e63f7c384a6de80e0c8f19edd96
piyushrajsingh092 <piyushrajsingh092@gmail.com>
2026-09-21 18:18:16 +0530

    chore: remove dist from git, add to .gitignore

--- backend/dist/video-engine/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const remotion_1 = require("remotion");
-const RemotionRoot_1 = require("./RemotionRoot");
-(0, remotion_1.registerRoot)(RemotionRoot_1.RemotionRoot);
-//# sourceMappingURL=index.js.map
